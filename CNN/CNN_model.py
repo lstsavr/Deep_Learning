31bbb3c0f008ec2f55d25b74ff7b1ec36ff48d03
此去惊年梦浪荡魂音 <150009325+lstsavr@users.noreply.github.com>
2025-03-04 14:34:59 +0800

    Update and rename torch.py to CNN_model.py

---
+++ CNN/CNN_model.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+
+class FaceCNN(nn.Module):
+    def __init__(self):
+        super(FaceCNN, self).__init__()
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(128 * 12 * 12, 7),
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = x.view(x.shape[0], -1)
+        return self.fc(x)
