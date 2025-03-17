1baa1f522cafff86fac66f068a023c6354763a3a
bTd <150009325+lstsavr@users.noreply.github.com>
2025-03-17 20:21:52 +0800

    Update emotion_map.py

--- CNN/emotion_map.py
+++ CNN/emotion_map.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 
 def image_emotion_mapping(image_dir):
-    # 读取 emotion.csv
+
     df_emotion = pd.read_csv('dataset/emotion.csv', header=None)
 
     files = [f for f in os.listdir(image_dir) if f.endswith(".jpg")]
@@ -15,10 +15,9 @@ def image_emotion_mapping(image_dir):
         path_list.append(file_name)
         emotion_list.append(df_emotion.iat[index, 0])  # 查 emotion
 
-    # 生成 DataFrame 并保存
     df_result = pd.DataFrame({'path': path_list, 'emotion': emotion_list})
     df_result.to_csv(os.path.join(image_dir, 'image_emotion.csv'), index=False, header=False)
 
-    print(f"{image_dir} 的 image_emotion.csv 生成成功！")
+    print(f"{image_dir} 's image_emotion.csv generate successfully！")
 
 image_emotion_mapping('face_images')
